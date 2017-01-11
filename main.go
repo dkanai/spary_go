dc13786f8b3fe6d195514c80c3726ff7ff493bee
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-01-11 21:54:06 +0900

    add kika-pc

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ func showOnsenList(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Onsen List")
 }
 
-func showOnsenList2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Onsen List2")
+func showKika(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pc")
 }
 
 func main() {
 	http.HandleFunc("/onsen/list", showOnsenList)
-	http.HandleFunc("/onsen/list2", showOnsenList2)
+	http.HandleFunc("/kika", showKika)
 	http.ListenAndServe(":8080", nil)
 }
