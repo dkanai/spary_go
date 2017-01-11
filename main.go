86018bdde6e829e3b1757d217d6aaf24ef0820a9
chiaki-yokoo <chiaki_yokoo@r.recruit.co.jp>
2017-01-11 21:54:31 +0900

    add showChiiia12

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ func showOnsenList(w http.ResponseWriter, r *http.Request) {
 func showOnsenList2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Onsen List2")
 }
+func showChiiia12(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"chiiia12")
+}
 
 func main() {
 	http.HandleFunc("/onsen/list", showOnsenList)
 	http.HandleFunc("/onsen/list2", showOnsenList2)
+	http.HandleFunc("/chiiia12",showChiiia12)
 	http.ListenAndServe(":8080", nil)
 }
