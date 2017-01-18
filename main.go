4855433424468e900c89002c50a70af7b3f13d57
k.kinoshita <k.kinoshita@pastoraldog.com>
2017-01-18 20:16:52 +0900

    modified error

--- main.go
+++ main.go
@@ -9,17 +9,21 @@ func showOnsenList(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Onsen List")
 }
 
+func showOnsenList2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Onsen List")
+}
+
 func showKika(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pc")
 }
-func showChiiia12(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintf(w,"chiiia12")
+func showChiiia12(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "chiiia12")
 }
 
 func main() {
 	http.HandleFunc("/onsen/list", showOnsenList)
 	http.HandleFunc("/onsen/list2", showOnsenList2)
-	http.HandleFunc("/chiiia12",showChiiia12)
+	http.HandleFunc("/chiiia12", showChiiia12)
 	http.HandleFunc("/kika", showKika)
 	http.ListenAndServe(":8080", nil)
 }
