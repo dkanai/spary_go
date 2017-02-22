c2863937781bfa5a319ebeec21987d54f14798f0
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-02-22 21:50:25 +0900

    use localhost mysql

--- db/init.sql
+++ db/init.sql
@@ -2,3 +2,4 @@ CREATE DATABASE spary;
 CREATE USER spary_admin IDENTIFIED BY 'spary@YRAPS';
 GRANT ALL PRIVILEGES ON spary.* TO 'spary_admin'@'%';
 FLUSH PRIVILEGES;
+-- grant all on spary.* to spary_admin@localhost identified by 'spary@YRAPS';
