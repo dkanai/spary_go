e444085d75f3bf28b375084281b97c7a5c2d4995
d.kanai <d.kanai@pastoraldog.com>
2017-03-02 01:14:42 +0900

    テストリファクタリング

--- main_test.go
+++ main_test.go
@@ -31,28 +31,35 @@ func truncateTable() {
   db.Query("DELETE FROM spa;")
 }
 
+func execShowSpaList() *api.Result {
+  //ShowSpaList実行
+	req, _ := http.NewRequest("GET", "v1/spas", nil)
+	res := httptest.NewRecorder()
+	api.ShowSpaList(res, req)
+
+  //レスポンスを構造体に変換
+	data, _ := ioutil.ReadAll(res.Body)
+	obj := new(api.Result)
+	json.Unmarshal(([]byte)(string(data)), obj)
+  return obj
+}
+
 func TestShowSpaList(t *testing.T) {
   db.Query("INSERT INTO spa (name, address) VALUES(?, ?)", "木下温泉", "北海道")
   db.Query("INSERT INTO spa (name, address) VALUES(?, ?)", "木下温泉2", "北海道2")
 
-	req, _ := http.NewRequest("GET", "/api/spa/list", nil)
-	res := httptest.NewRecorder()
-	api.ShowSpaList(res, req)
+  result := execShowSpaList()
 
-	data, _ := ioutil.ReadAll(res.Body)
-	obj := new(api.Result)
-	json.Unmarshal(([]byte)(string(data)), obj)
-
-	if obj.Spa[0].Name != "木下温泉" {
-		t.Fatalf("Data Error. %v != %v", string(obj.Spa[0].Name), "木下温泉")
+	if result.Spa[0].Name != "木下温泉" {
+		t.Fatalf("%v != %v", string(result.Spa[0].Name), "木下温泉")
 	}
-	if obj.Spa[0].Address != "北海道" {
-		t.Fatalf("Data Error. %v != %v", string(obj.Spa[0].Address), "北海道")
+	if result.Spa[0].Address != "北海道" {
+		t.Fatalf("%v != %v", string(result.Spa[0].Address), "北海道")
 	}
-	if obj.Spa[1].Name != "木下温泉2" {
-		t.Fatalf("Data Error. %v != %v", string(obj.Spa[1].Name), "木下温泉2")
+	if result.Spa[1].Name != "木下温泉2" {
+		t.Fatalf("%v != %v", string(result.Spa[1].Name), "木下温泉2")
 	}
-	if obj.Spa[1].Address != "北海道2" {
-		t.Fatalf("Data Error. %v != %v", string(obj.Spa[1].Address), "北海道2")
+	if result.Spa[1].Address != "北海道2" {
+		t.Fatalf("%v != %v", string(result.Spa[1].Address), "北海道2")
 	}
 }
