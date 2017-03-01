d853aac363917edcadbc2012e981600cb3a01ac4
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-03-01 21:43:38 +0900

    change uri

--- src/api/api.go
+++ src/api/api.go
@@ -80,7 +80,7 @@ func ShowSpaList(w http.ResponseWriter, r *http.Request) {
 }
 
 func Run() {
-	http.HandleFunc("/api/spa/list", ShowSpaList)
+	http.HandleFunc("/v1/spas", ShowSpaList)
 	fmt.Printf("Server is running... localhost:8080")
 	http.ListenAndServe(":8080", nil)
 
