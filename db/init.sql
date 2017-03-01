56d81d7dc2af077cd0520eee94e21e252e41f6a3
d.kanai <d.kanai@pastoraldog.com>
2017-03-01 21:35:40 +0900

    testENV用のDB接続情報を作成

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE spary;
+CREATE DATABASE spary_test;
 CREATE USER spary_admin IDENTIFIED BY 'spary@YRAPS';
 GRANT ALL PRIVILEGES ON spary.* TO 'spary_admin'@'%';
+GRANT ALL PRIVILEGES ON spary_test.* TO 'spary_admin'@'%';
 FLUSH PRIVILEGES;
 -- grant all on spary.* to spary_admin@localhost identified by 'spary@YRAPS';
